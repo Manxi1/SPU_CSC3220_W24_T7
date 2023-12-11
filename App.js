d607bd4c75f6df409563ec9cb3733cde980a035e
Manxi1 <muhirwag@spu.edu>
2023-12-11 15:13:56 -0800

    first screen
    no design yet
    
    Co-Authored-By: Weston Hanson <121981035+westonhanson@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,51 +1,25 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+import { StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { useNavigation } from '@react-navigation/native';
 
-const Drawer = createDrawerNavigator();
-
-function HomeScreen() {
-  const navigation = useNavigation();
-  const [isButtonPressed, setIsButtonPressed] = useState(false);
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.changefont}>Tracking</Text>
-      <TouchableOpacity onPress={() => navigation.openDrawer()} style={styles.menuButton}>
-        <Text style={styles.menuText}>☰</Text>
-      </TouchableOpacity>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity 
-          style={[
-            styles.roundButton,
-            isButtonPressed ? styles.buttonPressed : styles.buttonNotPressed,
-          ]}
-          onPressIn={() => setIsButtonPressed(true)}
-          onPressOut={() => setIsButtonPressed(false)}
-          onPress={() => console.log('Button pressed')}
-        >
-          <Text style={styles.buttonText}>+</Text>
-        </TouchableOpacity>
-      </View>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React from 'react';
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
+      return(
+        <View style={styles.container}>
+          <Text style={styles.changefont}>Tracking</Text>
+          <TouchableOpacity onPress={() => console.log('Button pressed')} style={styles.menuButton}>
+          <Text style={styles.menuText}>☰</Text>
+           </TouchableOpacity>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.roundButton} onPress={() => console.log('Button pressed')}>
+              <Text style={styles.buttonText}>+</Text>
+            </TouchableOpacity>
+          </View>
+          <StatusBar style="auto" />
+        </View>
+      );
 }
-
-// ... rest of your code
 
 const styles = StyleSheet.create({
   container: {
@@ -82,33 +56,13 @@ const styles = StyleSheet.create({
   },
   menuButton: {
     position: 'absolute',
-    top: 48,
+    top: 50,
     left: 20,
   },
   
   menuText: {
     fontSize: 30,
     color: '#000',
-  },
-  buttonPressed: {
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  
-  buttonNotPressed: {
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 0,
-    },
-    shadowOpacity: 0,
-    shadowRadius: 0,
-    elevation: 0,
+    
   },
 });
